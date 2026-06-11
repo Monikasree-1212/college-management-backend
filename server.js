ed0459b451d29ee495d042fa5d746ccb2e4e228f
Monikasree-1212 <monikasree.d2024cce@sece.ac.in>
2026-06-11 22:00:00 +0530

    Added Vercel frontend URL to CORS

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173','https://college-management-frontend-tau.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
